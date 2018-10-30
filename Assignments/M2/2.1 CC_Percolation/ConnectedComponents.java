11da2ee9b59fc8635615c1c5a0734abebc084a8b
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-10-30 18:41:01 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/M2/2.1 CC_Percolation/ConnectedComponents.java
+++ Assignments/M2/2.1 CC_Percolation/ConnectedComponents.java
@@ -1,53 +1,112 @@
-class ConnectedComponents {
+public class ConnectedComponents {
+    private boolean[] marked;   // marked[v] = has vertex v been marked?
+    private int[] id;           // id[v] = id of connected component containing v
+    private int[] size;         // size[id] = number of vertices in given component
+    private int count;          // number of connected components
+
     /**
-     * declaration of boolean array.
-     */
-    private boolean[] marked;
-    /**
-     * declaration of variable.
-     */
-    private int[] id;
-    /**
-     * declaration of variable.
-     */
-    private int count;
-    /**
-     * Constructs the object.
+     * Computes the connected components of the undirected graph {@code G}.
      *
-     * @param      g     { parameter_description }
-     * @param      s     { parameter_description }
+     * @param G the undirected graph
      */
-    ConnectedComponents(final Graph g, final int s) {
-        marked = new boolean[g.vertices()];
-        id  = new int[g.vertices()];
-        for (int i = 0; i < g.vertices(); i++) {
-            marked[i] = false;
-            if (!marked[i]) {
-                dfs(g, i);
+    public ConnectedComponents(Graph G) {
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        size = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
                 count++;
             }
         }
     }
-    public int count() {
-        return count;
-    }
-    public int id(int v) {
-        return id[v];
-    }
-    private void dfs(Graph g, int v) {
+
+    // depth-first search for a Graph
+    private void dfs(Graph G, int v) {
         marked[v] = true;
         id[v] = count;
-        for (int each : g.adj(v)) {
-            if (!marked[each]) {
-                dfs(g, each);
+        size[count]++;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
             }
         }
     }
-    public boolean percolates() {
-        if (count > 1) {
-            return false;
-        } else {
-            return true;
-        }
+
+
+    /**
+     * Returns the component id of the connected component containing vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the component id of the connected component containing vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int id(int v) {
+        validateVertex(v);
+        return id[v];
+    }
+
+    /**
+     * Returns the number of vertices in the connected component containing vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the number of vertices in the connected component containing vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int size(int v) {
+        validateVertex(v);
+        return size[id[v]];
+    }
+
+    /**
+     * Returns the number of connected components in the graph {@code G}.
+     *
+     * @return the number of connected components in the graph {@code G}
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns true if vertices {@code v} and {@code w} are in the same
+     * connected component.
+     *
+     * @param  v one vertex
+     * @param  w the other vertex
+     * @return {@code true} if vertices {@code v} and {@code w} are in the same
+     *         connected component; {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @throws IllegalArgumentException unless {@code 0 <= w < V}
+     */
+    public boolean connected(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return id(v) == id(w);
+    }
+
+    /**
+     * Returns true if vertices {@code v} and {@code w} are in the same
+     * connected component.
+     *
+     * @param  v one vertex
+     * @param  w the other vertex
+     * @return {@code true} if vertices {@code v} and {@code w} are in the same
+     *         connected component; {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @throws IllegalArgumentException unless {@code 0 <= w < V}
+     * @deprecated Replaced by {@link #connected(int, int)}.
+     */
+    @Deprecated
+    public boolean areConnected(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return id(v) == id(w);
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
     }
 }
--- Assignments/M2/2.1 CC_Percolation/Solution.java
+++ Assignments/M2/2.1 CC_Percolation/Solution.java
@@ -1,19 +1,173 @@
 import java.util.Scanner;
-class Solution {
-	Solution() {
+/**
+ * Class for percolation.
+ */
+class Percolation {
+    /**
+     *the boolean array.
+     */
+    private boolean[] array;
+    /**
+     *object declaration.
+     */
+    private Graph graph;
+    /**
+     * variable for array size.
+     */
+    private int arraySize;
+    /**
+     * variable for size.
+     */
+    private int size;
+    /**
+     * initializing count.
+     */
+    private int count;
+    /**
+     *first variable.
+     */
+    private int first;
+    /**
+     * last variable.
+     */
+    private int last;
+    /**
+     * Constructs the object.
+     */
+    protected Percolation() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = Integer.parseInt(sc.nextLine());
-		GraphForm graph = new GraphForm(size * size);
-		while(sc.hasNext()) {
-			String[] links = sc.nextLine().split(" ");
-			graph.addEdge(Integer.parseInt(links[0]),
-            Integer.parseInt(links[1]));
-            //System.out.println(graph.hasEdge((Integer.parseInt(links[0]) - 1), Integer.parseInt(links[1]) - 1));
-		}
-		ConnectedComponents obj = new ConnectedComponents(graph, (size));
-		System.out.println(obj.percolates());
-	}
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param n int
+     */
+    Percolation(final int n) {
+        this.arraySize = n;
+        this.size = n * n;
+        this.first = size;
+        this.last = size + 1;
+        this.count = 0;
+        graph = new Graph(size + 2);
+        array = new boolean[size];
+        for (int i = 0; i < arraySize; i++) {
+            graph.addEdge(first, i);
+            graph.addEdge(last, size - i - 1);
+        }
+    }
+    /**
+     * method to convert from two dimensional to one dimensional.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return  onedimensional array
+     */
+    public int toOneD(final int row, final int col) {
+        return (arraySize * (row - 1)) + (col - 1);
+    }
+    /**
+     * Connects open sites(== full site).
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    private void connectOpenSites(final int row, final int col) {
+        if (array[col] && !graph.hasEdge(row, col)) {
+            graph.addEdge(row, col);
+        }
+    }
+    /**
+     * method that opens the blocked site.
+     *
+     * @param      row     The row
+     * @param      col  The column
+     */
+    public void open(final int row, final int col) {
+        int index = toOneD(row, col);
+        array[index] = true;
+        count++;
+        int firstrow = index - arraySize;
+        int lastrow = index + arraySize;
+        if (arraySize == 1) {
+            graph.addEdge(first, index);
+            graph.addEdge(last, index);
+        }
+        if (lastrow < size) {         //last
+            connectOpenSites(index, lastrow);
+        }
+        if (firstrow >= 0) {              //first
+            connectOpenSites(index, firstrow);
+        }
+        if (col == 1) {                 //left
+            if (col != arraySize) {
+                connectOpenSites(index, index + 1);
+            }
+            return;
+        }
+        if (col == arraySize) {         //right
+            connectOpenSites(index, index - 1);
+            return;
+        }
+        connectOpenSites(index, index + 1);
+        connectOpenSites(index, index - 1);
+    }
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return array[toOneD(row, col)];
+    }
+    /**
+     * return number of open sites.
+     *
+     * @return count
+     */
+    public int numberOfOpenSites() {
+        return count;
+    }
+    /**
+     * method to check whether there is a flow.
+     *
+     * @return boolean
+     */
+    public boolean percolates() {
+        ConnectedComponents cc = new ConnectedComponents(graph);
+        return cc.connected(first, last);
+    }
+}
+
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
+        //unused constructor.
+    }
+
+    /**
+     * Client program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Percolation pobj = new Percolation(n);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            pobj.open(Integer.parseInt(tokens[0]),
+                      Integer.parseInt(tokens[1]));
+        }
+        System.out.println(pobj.percolates()
+                           && pobj.numberOfOpenSites() != 0);
+    }
 }
