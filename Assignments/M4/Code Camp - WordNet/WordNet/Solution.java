b3c96342b10dca62219a3c29a8aee6796039da49
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-11-04 23:24:03 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/M4/Code Camp - WordNet/WordNet/Solution.java
+++ Assignments/M4/Code Camp - WordNet/WordNet/Solution.java
@@ -15,10 +15,17 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         String synsetsFileName = StdIn.readLine();
+        // In in = new In(str);
+        // System.out.println(strsynsetsFileName + "lol");
         String hypernymsFileName = StdIn.readLine();
+        // In in1 = new In(str1);
+        // System.out.println(hypernymsFileName + "lollll");
+        // System.out.println(wn.nouns());
         String str2 = StdIn.readLine();
         switch (str2) {
+        // WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
         case "Graph":
+            // System.out.println(wn.getDg().toString());
             try {
                 WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
                 wn.display();
@@ -27,9 +34,11 @@ public final class Solution {
             }
             break;
         case "Queries":
+            // System.out.println("lll");
             try {
                 WordNet wnq = new WordNet(synsetsFileName,
                                           hypernymsFileName);
+                // System.out.println(line + "ooo");
                 while (StdIn.hasNextLine()) {
                     String line = StdIn.readLine();
                     String[] strarr = line.split(" ");
@@ -50,3 +59,4 @@ public final class Solution {
         }
     }
 }
+
