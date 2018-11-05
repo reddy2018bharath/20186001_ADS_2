bd58464ba40751f6bbc376ba43a3376af9547fa9
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-11-05 18:57:10 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/M7/1. MST/Solution.java
+++ Assignments/M7/1. MST/Solution.java
@@ -1,36 +1,39 @@
 /**
- * { item_description }
+ * { item_description }.
  */
 import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 /**
  * Constructs the object.
  */
-	private Solution() {
+    private Solution() {
 
-	}
+    }
 /**
- * { function_description }
+ * { function_description }.
  *
  * @param      args  The arguments
  */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int v = Integer.parseInt(sc.nextLine());
-		int e = Integer.parseInt(sc.nextLine());
-		EdgeWeightedGraph weightedGraph = new EdgeWeightedGraph(v);
-		for (int i = 0; i < e; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			Edge edge = new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
-			weightedGraph.addEdge(edge);
-		}
-		KruskalMST kmst = new KruskalMST(weightedGraph);
-		System.out.printf("%.5f\n", kmst.weight());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int v = Integer.parseInt(sc.nextLine());
+        int e = Integer.parseInt(sc.nextLine());
+        EdgeWeightedGraph weightedGraph = new EdgeWeightedGraph(v);
+        for (int i = 0; i < e; i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            Edge edge = new Edge(Integer.parseInt(
+                tokens[0]), Integer.parseInt(
+                tokens[1]), Double.parseDouble(tokens[2]));
+            weightedGraph.addEdge(edge);
+        }
+        KruskalMST kmst = new KruskalMST(weightedGraph);
+        System.out.printf("%.5f\n", kmst.weight());
 
-		
+        
 
-	}
+    }
 }
+
