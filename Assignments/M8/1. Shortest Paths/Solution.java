d8573e3e32122ffb325f2f411db75613cbf4dc2b
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-11-12 17:51:44 +0530

    "ads2_m8_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ Assignments/M8/1. Shortest Paths/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.HashMap;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // unused Constructor.
+    }
+    /**
+     * {Client Program}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] tokens = sc.nextLine().split(" ");
+        int nameOfStations = Integer.parseInt(tokens[0]);
+        int m = Integer.parseInt(tokens[1]);
+        String[] check = sc.nextLine().split(" ");
+        EdgeWeightedGraph edgeWeightedGraph =
+        new EdgeWeightedGraph(nameOfStations);
+        HashMap<String, Integer> hash = new HashMap<String, Integer>();
+        for (int i = 0; i < check.length; i++) {
+            hash.put(check[i], i);
+        }
+        while (m > 0) {
+            String[] line = sc.nextLine().split(" ");
+            edgeWeightedGraph.addEdge(new Edge(
+                            hash.get(line[0]), hash.get(line[1]),
+                            Double.parseDouble(line[2])));
+        }
+        int queries = Integer.parseInt(sc.nextLine());
+        while (queries > 0) {
+            String[] check1 = sc.nextLine().split(" ");
+            int a = hash.get(check1[0]);
+            DijkstraUndirectedSP dijkstra =
+            new DijkstraUndirectedSP(edgeWeightedGraph, a);
+            if (dijkstra.hasPathTo(
+                hash.get(check1[1]))) {
+                System.out.println((int)
+                    dijkstra.distTo(hash.get(check1[1])));
+            }
+            queries--;
+        }
+    }
+}
+
+
+
+
+
---
+++ Assignments/M8/1. Shortest Paths/Stack.java
@@ -0,0 +1,158 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ * List of .
+ *
+ * @param      <Item>  The item
+ */
+public class Stack<Item> implements Iterable<Item> {
+    /**
+     * { var_description }.
+     */
+    private int size;          // size of the stack
+    /**
+     * { var_description }.
+     */
+    private Node first;     // top of stack
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * { var_description }.
+         */
+        private Item item;
+        /**
+         * { var_description }.
+         */
+        private Node next;
+    }
+
+    /**
+      * Create an empty stack.
+      */
+    public Stack() {
+        first = null;
+        size = 0;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * size.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int size() {
+        return size;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      item  The item
+     */
+    public void push(final Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        size++;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Item pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
+        Item item = first.item;        // save item to return
+        first = first.next;            // delete first node
+        size--;
+        return item;                   // return the saved item
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Item peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
+        return first.item;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item + " ");
+        }
+        return s.toString();
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator();
+    }
+    /**
+     * Class for list iterator.
+     */
+    private class ListIterator implements Iterator<Item> {
+        /**
+         * { var_description }.
+         */
+        private Node current = first;
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext()  {
+            return current != null;
+        }
+        /**
+         * { function_description }.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * { function_description }.
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Item next() {
+            if (!hasNext()) {
+                    throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
